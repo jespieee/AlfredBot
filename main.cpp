634e271dd8e648fc1c88f6f86e70beffe1894ebb
mhuhu <mhuhu@umich.edu>
2022-06-10 19:02:49 -0400

    Starter files with function stubs

---
+++ main.cpp
@@ -0,0 +1,9 @@
+// Project UID db1f506d06d84ab787baf250c265e24e
+
+#include "csvstream.h"
+#include "Map.h"
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "hello from main!\n";
+}
