c6d0a1a253582859a9bd72d7e4aec397abf1626d
mhuhu <mhuhu@umich.edu>
2022-06-17 16:23:02 -0400

    aaron's commit

--- BinarySearchTree_tests.cpp
+++ BinarySearchTree_tests.cpp
@@ -11,6 +11,21 @@ TEST(test_bst_ctor) {
     ASSERT_EQUAL(tree.height(), 0);
 }
 
+TEST(test_empty) {
+    BinarySearchTree<int> tree;
+    ASSERT_TRUE(tree.empty());
+    tree.insert(7);
+    ASSERT_FALSE(tree.empty());
+}
+
+TEST(test_size) {
+    BinarySearchTree<int> tree;
+    ASSERT_EQUAL(tree.size(), 0);
+    tree.insert(7);
+    ASSERT_FALSE(tree.empty());
+    ASSERT_EQUAL(tree.size(), 1);
+}
+
 TEST(test_bst_cpy) {
     BinarySearchTree<int> tree;
     tree.insert(3);
@@ -23,7 +38,15 @@ TEST(test_bst_cpy) {
 }
 
 TEST(test_find) {
-
+    BinarySearchTree<int> tree;
+    tree.insert(3);
+    ASSERT_EQUAL(*tree.find(3), 3);
+    tree.insert(7);
+    tree.insert(5);
+    tree.insert(1);
+    ASSERT_EQUAL(*tree.find(5), 5);
+    ASSERT_EQUAL(*tree.find(7), 7);
+    ASSERT_EQUAL(*tree.find(1), 1);
 }
 
 TEST(test_height1) {
@@ -56,4 +79,77 @@ TEST(test_height2) {
     ASSERT_EQUAL(tree.height(), 4);
 }
 
+/*TEST(test_inorder_preorder) {
+    BinarySearchTree<int> tree;
+    tree.insert(5);
+    tree.insert(3);
+    tree.insert(4);
+    tree.insert(1);
+    tree.insert(7);
+    tree.insert(6);
+    ostringstream os;
+    ostringstream os1;
+    tree.traverse_inorder(os); // inorder, 134567
+    ASSERT_EQUAL(os.str(), "1 3 4 5 6 7 ")
+    tree.traverse_preorder(os1); // preorder, 531476
+    ASSERT_EQUAL(os1.str(), "5 3 1 4 7 6 ")
+}
+
+TEST(test_preorder2) {
+    BinarySearchTree<int> tree;
+    tree.insert(8);
+    tree.insert(6);
+    tree.insert(7);
+    tree.insert(12);
+    tree.insert(9);
+    tree.insert(13);
+    tree.traverse_preorder(cout); // preorder, 867 12 9 13
+}*/
+
+TEST(test_insert) {
+    BinarySearchTree<int> tree;
+    tree.insert(4);
+    ASSERT_EQUAL(tree.size(), 1);
+    ASSERT_EQUAL(*tree.find(4), 4);
+}
+
+TEST(test_min_max) {
+    BinarySearchTree<int> tree;
+    tree.insert(4);
+    tree.insert(8);
+    tree.insert(7);
+    tree.insert(12);
+    ASSERT_EQUAL(*tree.min_element(), 4);
+    ASSERT_EQUAL(*tree.max_element(), 12);
+    BinarySearchTree<int> tree1;
+    tree1.insert(4);
+    ASSERT_EQUAL(*tree1.min_element(), 4);
+    ASSERT_EQUAL(*tree1.max_element(), 4);
+}
+
+TEST(test_min_greater_than) {
+    BinarySearchTree<int> tree;
+    tree.insert(12);
+    tree.insert(7);
+    tree.insert(11);
+    tree.insert(5);
+    tree.insert(6);
+    tree.insert(33);
+    ASSERT_EQUAL(*tree.min_greater_than(5), 6);
+    ASSERT_EQUAL(*tree.min_greater_than(6), 7);
+    ASSERT_EQUAL(*tree.min_greater_than(12), 33);
+}
+
+TEST(test_check_sort) {
+    BinarySearchTree<int> tree;
+    ASSERT_TRUE(tree.check_sorting_invariant());
+    tree.insert(16);
+    tree.insert(7);
+    tree.insert(11);
+    ASSERT_TRUE(tree.check_sorting_invariant());
+    tree.insert(5);
+    tree.insert(6);
+    tree.insert(8);
+    ASSERT_TRUE(tree.check_sorting_invariant());
+}
 TEST_MAIN()
--- main.cpp
+++ main.cpp
@@ -1,9 +1,272 @@
 // main.cpp
 // Project UID db1f506d06d84ab787baf250c265e24e
 
+#include <cassert>
 #include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <string>
+#include <utility>
+#include <map>
+#include <set>
+#include <cmath>
+#include "csvstream.h"
 using namespace std;
 
-int main() {
-	cout << "hello from main!\n";
+class PiazzaBot {
+private:
+	double numPosts;
+	double vocabSize;
+	map<string, double> words;
+	map<string, double> labels;
+	map<pair<string, string>, double> finalMap; // For each label C and word W, 
+											    // num posts with label C that contain W
+public:
+	PiazzaBot() {
+		numPosts = 0;
+		vocabSize = 0;
+	}
+	// R: file is valid
+	// M: all private member variables of PiazzaBot
+	// E: trains PiazzaBot using all smaller training functions
+	bool Train(const string& str, bool wantDebug, bool isOpen) {
+		if (!isOpen) {
+			return true;
+		}
+		ifstream a;
+		ifstream b;
+		ifstream c;
+		ifstream d;
+		ifstream e;
+		a.open(str);
+		b.open(str);
+		c.open(str);
+		d.open(str);
+		e.open(str);
+		if (!(a.is_open() && b.is_open() && c.is_open()
+			&& d.is_open() && e.is_open())) {
+			return false;
+		}
+		csvstream trainPost(e);
+		csvstream trainVocab(d);
+		csvstream trainWords(c);
+		csvstream trainLabels(b);
+		csvstream trainfinalMap(a);
+		post_train(trainPost, wantDebug);
+		vocab_train(trainVocab, wantDebug);
+		words_train(trainWords);
+		labels_train(trainLabels, wantDebug);
+		finalMap_train(trainfinalMap, wantDebug);		
+		return true;
+	}
+
+	void Predict(csvstream& file) {
+		map<string, string> row;
+		set<string> bagOfVocab = unique_words(row["content"]);
+		map<double, string> Logs;
+		vector<double> logVector;
+		int guesses = 0;
+		int correctGuesses = 0;
+		while (file >> row) {
+			for (auto i = labels.begin(); i != labels.end(); ++i) {
+				double logPerLabel = calc_log_prior(i->first);
+				for (auto j = bagOfVocab.begin(); j != bagOfVocab.end(); ++j) {
+					logPerLabel += calc_log_likelihood(i->first, *j);
+				}
+				if (Logs.find(logPerLabel) == Logs.end()) {
+					pair<double, string> p1(logPerLabel, i->first);
+					Logs.insert(p1);
+					logVector.push_back(logPerLabel);
+				}
+			}			
+			double max = *max_element(logVector.begin(), logVector.end());
+			string prediction = Logs[max];
+			if (prediction == row["tag"]) {
+				correctGuesses++;
+			}
+			guesses++;
+			cout << "test data:\n";
+			cout << "  correct = " << row["tag"] << ", predicted = "
+				<< prediction << ", log-probability score = " << max << "\n";
+			cout << "  content = " << row["content"] << "\n\n";
+		}
+		cout << "performance: " << correctGuesses << " / " << guesses
+			<< " posts predicted correctly\n";
+	}
+	// R: str is valid
+	// M: N/A
+	// E: returns log prior
+	double calc_log_prior(const string& str) {
+		double numPostsC = labels[str];
+		return log(numPostsC / numPosts);
+	}
+	// R: str and finder are valid
+	// M: N/A
+	// E: returns log likelihood in three scenarios
+	double calc_log_likelihood(const string& str, const string& finder) {
+		pair<string, string> p1(str, finder);
+		double numPostsW = words[finder];
+		double numPostsC = labels[str];
+		double numPostsCW = finalMap[p1];
+		if (words.find(finder) == words.end()) {
+			return log(1 / numPosts);
+		}
+		else if (finalMap.find(p1) == finalMap.end()) {
+			return log(numPostsW / numPosts);
+		}
+		else {
+			return log(numPostsCW / numPostsC);
+		}
+	}
+
+	set<string> unique_words(const string& str) {
+		istringstream source(str);
+		set<string> words;
+		string word;
+		// Read word by word from the stringstream and insert into the set
+		while (source >> word) {
+			words.insert(word);
+		}
+		return words;
+	}
+	// R: file is valid
+	// M: numPosts
+	// E: trains PiazzaBot on X amount of posts
+	void post_train(csvstream& file, bool wantDebug) {
+		if (wantDebug) {
+			cout << "training data:" << endl;
+		}
+		map<string, string> debugRow;
+		while (file >> debugRow) {
+			if (wantDebug) {
+				numPosts++;
+				cout << "  label = " << debugRow["tag"] << ", content = "
+					<< debugRow["content"] << "\n";
+			}
+		}
+		cout << "trained on " << numPosts << " examples\n";
+	}
+	// R: file is valid
+	// M: vocabSize
+	// E: trains PiazzaBot on all unique vocab
+	void vocab_train(csvstream& file, bool wantDebug) {
+		map<string, string> debugRow;
+		set<string> allVocabSet;
+		string allVocab = "";
+		while (file >> debugRow) {
+			allVocab += " " + debugRow["content"];
+		}
+		allVocabSet = unique_words(allVocab);
+		vocabSize = allVocabSet.size();
+		if (wantDebug) {
+			cout << "vocabulary size = " << vocabSize << "\n\n";
+		}
+	}
+	// R: file is valid
+	// M: words map
+	// E: trains PiazzaBot on words, stores in a map
+	void words_train(csvstream& file) {
+		map<string, string> row;
+		string allVocab = "";
+		set<string> allVocabSet;
+		while (file >> row) {
+			allVocabSet = unique_words(row["content"]);
+			for (auto i = allVocabSet.begin(); i != allVocabSet.end(); ++i) {
+				if (words.find(*i) != words.end()) {
+					words[*i] += 1;
+				}
+				else {
+					pair<string, int> p1(*i, 1);
+					words.insert(p1);
+				}
+			}
+		}
+	}
+	// R: file is valid
+	// M: labels map
+	// E: trains PiazzaBot on labels, stores in a map
+	void labels_train(csvstream& file, bool wantDebug) {
+		map<string, string> debugRow;
+		while (file >> debugRow) {
+			if (labels.find(debugRow["tag"]) != labels.end()) {
+				auto i = labels.find(debugRow["tag"]);
+				i->second += 1;
+			}
+			else {
+				pair<string, double> p1(debugRow["tag"], 1);
+				labels.insert(p1);
+			}
+		}
+		if (wantDebug) {
+			cout << "classes:\n";
+			for (auto i = labels.begin(); i != labels.end(); ++i) {
+				cout << "  " << i->first << ", "
+					 << i->second << " examples, log-prior = "
+					 << calc_log_prior(i->first) << "\n";
+			}
+		}
+	}
+	// R: file is valid
+	// M: labels map
+	// E: trains PiazzaBot on labels, stores in a map
+	void finalMap_train(csvstream& file, bool wantDebug) {
+		map<string, string> debugRow;
+		set<string> allVocabSet;
+		while (file >> debugRow) {
+			allVocabSet = unique_words(debugRow["content"]);
+			for (auto i = allVocabSet.begin(); i != allVocabSet.end(); ++i) {
+				pair<string, string> p1(debugRow["tag"], *i);
+				if (finalMap.find(p1) != finalMap.end()) {
+					finalMap.find(p1)->second += 1;
+				}
+				else {
+					pair<pair<string, string>, int> p2(p1, 1);
+					finalMap.insert(p2);
+				}
+			}
+		}
+		if (wantDebug) {
+			cout << "classifier parameters:\n";
+			for (auto i = finalMap.begin(); i != finalMap.end(); ++i) {
+				if (i->second != 0) {
+					cout << "  " << i->first.first << ":"
+						 << i->first.second << ", count = "
+						 << i->second << ", log-likelihood = "
+						 << calc_log_likelihood(i->first.first, i->first.second)
+						 << "\n";
+				}
+			}
+		}
+	}
+}; // End class PiazzaBot
+
+void print_error() {
+	cout << "Usage: main.exe TRAIN_FILE TEST_FILE [--debug]" << endl;
 }
+
+void file_error(string file) {
+	cout << "Error opening file: " << file << endl;
+}
+
+int main(int argc, char** argv) {
+	bool wantDebug;
+	ifstream file;
+	cout.precision(3);
+	string DB = argv[3];
+	string openString = argv[2];
+	if (argc != 3 && argc != 4) {
+		print_error();
+		return 1;
+	}
+	
+	else if (argc == 4 && DB != "--debug") {
+		print_error();
+		return 1;
+	}
+	else if (argc == 4 && DB != "--debug") {
+		wantDebug = true;
+	}
+	PiazzaBot PiazzaBot;
+	file.open(openString);
+
+} 
